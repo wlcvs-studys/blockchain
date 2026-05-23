852803edc78e242415f4c89959f4f5319b384787
Wallacy V. da Silva <wlcsv8080@outlook.com>
2026-05-23 19:54:56 -0300

    Transactions endpoint created.

--- api.py
+++ api.py
@@ -1,6 +1,6 @@
 from textwrap import dedent
 from uuid import uuid4
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 from blockchain import Blockchain
 
@@ -19,7 +19,19 @@ def mine():
 
 @app.route('/transaction/new', methods = ['POST'])
 def new_transaction():
-  return "We'll add a new transaction"
+  values = request.get_json()
+
+  # Check that the required fields are in the POST'ed data 
+  required = ['sender', 'recipient', 'amount']
+  if not all(k in values for k in required):
+    return 'Missing values', 400 
+  
+  # Create a new Transaction
+  index = blockchain.new_transaction(values['sender'], values['recipient'], values['amount'])
+
+  response = {'message': f'Transaction will be added to Block {index}'}
+
+  return jsonify(response), 201
 
 @app.route('/chain', methods = ['GET'])
 def full_chain():
