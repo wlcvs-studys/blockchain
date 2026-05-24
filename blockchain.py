991dd77fbbdeaf7b690a5726d79a98ef5c6df6f1
Wallacy V. da Silva <wlcsv8080@outlook.com>
2026-05-24 02:02:52 -0300

    Consensus Algorithm.

--- blockchain.py
+++ blockchain.py
@@ -3,21 +3,102 @@ import json
 
 from time import time
 
+from urllib.parse import urlparse
+import requests
+
 class Blockchain:
   def __init__(self):
     self.current_transactions = []
     self.chain = []
+    self.nodes = set()
 
     # Create the genesis block
     self.new_block(previous_hash = 1, proof = 100)
 
+  def register_node(self, address):
+    """
+    Add a new node to the list of nodes
+
+    :parm address: <str> Address of node. Eg. 'http://192.168.0.5:5000'
+
+    :return: None
+    """
+
+    parsed_url = urlparse(address)
+    self.nodes.add(parsed_url.netloc)
+
+  def valid_chain(self, chain):
+    """
+    Determine if a given blockchain is valid
+
+    :param chain: <list> A blockchain
+    :return: <bool> True if valid, False if not
+    """
+
+    last_block = chain[0]
+    current_index = 1
+
+    while current_index < len(chain):
+      block = chain[current_index]
+      print(f'{last_block}')
+      print(f'{block}')
+      print("\n-----------\n")
+      # Check that the hash of the block is correct
+      if block['previous_hash'] != self.hash(last_block):
+        return False
+
+      # Check that the Proof of Work is correct
+      if not self.valid_proof(last_block['proof'], block['proof']):
+        return False
+
+      last_block = block
+      current_index += 1
+
+    return True
+
+  def resolve_conflicts(self):
+    """
+    This is our Consensus Algorithm, it resolves conflicts
+    by replacing our chain with the longest one in the network.
+
+    :return: <bool> True if our chain was replaced, False if not
+    """
+
+    neighbours = self.nodes
+    new_chain = None
+
+    # We're only looking for chains longer than ours
+    max_length = len(self.chain)
+
+    # Grab and verify the chains from all the nodes in our network
+    for node in neighbours:
+      response = requests.get(f'http://{node}/chain')
+
+      if response.status_code == 200:
+        length = response.json()['length']
+        chain = response.json()['chain']
+
+        # Check if the length is longer and the chain is valid
+        if length > max_length and self.valid_chain(chain):
+          max_length = length
+          new_chain = chain 
+
+    # Replace our chain if we discovered a new, valid chain longer than ours
+    if new_chain: 
+      self.chain = new_chain
+      return True
+
+    return False
+
   def new_block(self, proof, previous_hash = None):
     """
     Create a new Block in the Blockchain
+
     :param proof: <int> The proof given by the Proof of Work algorithm
     :param previous_hash: (Optional) <str> Hash of previous Block
     :return: <dict> New Block
     """
+
     block = {
       'index': len(self.chain) + 1,
       'timestamp': time(),
@@ -35,11 +116,13 @@ class Blockchain:
   def new_transaction(self, sender, recipient, amount):
     """
     Creates a new transaction to go into the next mined Block
+
     :param sender: <str> Address of the Sender
     :param recipient: <str> Address of the Recipient
     :param amount: <int> Amount
     :return: <int> The index of the Block that will hold this transaction
     """
+
     self.current_transactions.append({
       'sender': sender,
       'recipient': recipient,
@@ -56,6 +139,7 @@ class Blockchain:
   def hash(block):
     """
     Creates a SHA-256 hash of a Block
+
     :param block: <dict> Block
     :return: <str>
     """
@@ -68,9 +152,11 @@ class Blockchain:
   def proof_of_work(self, last_proof):
     """
     Simple Proof of Work Algorithm:
+
       - Find a number p' such that hash(pp') contains leading 4 zeroes,
         where p is the previous p' 
       - p is the previous proof, and p' is the new proof 
+
       :param last_proof: <int>
       :return: <int>
     """
@@ -85,6 +171,7 @@ class Blockchain:
   def valid_proof(last_proof, proof):
     """
     Validates the Proof: Does hash(last_proof, proof) contain 4 leading zeroes?
+
     :param last_proof: <int> Previous Proof
     :param proof: <int> Current Proof
     :return: <bool> True if correct, False if not.
@@ -94,4 +181,3 @@ class Blockchain:
     guess_hash = hashlib.sha256(guess).hexdigest()
     
     return guess_hash[:4] == "0000"
-
